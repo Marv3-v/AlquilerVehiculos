c856a930c4c7986afa5aa747d4686e81436d2b1b
Marvin <marvi4alarcon3@gmail.com>
2020-04-03 09:42:31 -0600

    Actualizada animacion para que dure menos tiempo, index

--- js/script.js
+++ js/script.js
@@ -1,48 +1,31 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var MedioTransporte = /** @class */ (function () {
-    function MedioTransporte(medio, tipo) {
+class MedioTransporte {
+    constructor(medio, tipo) {
         this.medio = medio;
         this.tipo = tipo;
     }
-    MedioTransporte.prototype.getTipo = function () {
+    getTipo() {
         return this.tipo;
-    };
-    return MedioTransporte;
-}());
-//const terrestre = new MedioTransporte('Terrestre');
+    }
+}
+//const terrestre = new MedioTransporte('Terrestre', 'Carrp');
 //const aereo = new MedioTransporte('Aereo');
 //const maritimo = new MedioTransporte('Maritimo');
-//console.log(medio.tipo);
-var Vehiculo = /** @class */ (function (_super) {
-    __extends(Vehiculo, _super);
+//console.log(terrestre);
+class Vehiculo extends MedioTransporte {
     //
-    function Vehiculo(medio, tipo, anio, motor, cantP, color, formato, plazo) {
-        var _this = _super.call(this, medio, tipo) || this;
-        _this.anio = anio;
-        _this.motor = motor;
-        _this.cantP = cantP;
-        _this.color = color;
-        _this.color = color;
-        _this.formato = formato;
-        _this.plazo = plazo;
-        return _this;
+    constructor(medio, tipo, anio, motor, cantP, color, formato, plazo) {
+        super(medio, tipo);
+        this.anio = anio;
+        this.motor = motor;
+        this.cantP = cantP;
+        this.color = color;
+        this.color = color;
+        this.formato = formato;
+        this.plazo = plazo;
     }
-    return Vehiculo;
-}(MedioTransporte));
+}
 function rentar() {
-    var ve = new Vehiculo(localStorage.getItem('medioTransporte'), localStorage.getItem('Aereo'), document.querySelector('#anio').value, document.querySelector('#motor').value, parseInt(document.querySelector('#cant').value), document.querySelector('#color').value, document.querySelector('#formato').value, parseInt(document.querySelector('#plazo').value));
+    let ve = new Vehiculo(localStorage.getItem('medioTransporte'), localStorage.getItem('Aereo'), document.querySelector('#anio').value, document.querySelector('#motor').value, parseInt(document.querySelector('#cant').value), document.querySelector('#color').value, document.querySelector('#formato').value, parseInt(document.querySelector('#plazo').value));
     localStorage.setItem('Objeto', JSON.stringify(ve));
     window.location.href = '/detalle.html';
 }
